73e47eee773362b6ae045e8d69540ff121ff8cf4
andreasessa <andrea.sessa1995libero.it>
2020-02-10 18:17:12 +0100

    Domande e quesito più debug

---
+++ app/src/main/java/com/example/threemath/Domanda.java
@@ -0,0 +1,52 @@
+package com.example.threemath;
+
+import java.util.ArrayList;
+
+public class Domanda {
+static String domanda ="";
+static String rispostaEsatta ="";
+
+static String rispostaErrata1;
+static String rispostaErrata2;
+static String rispostaErrata3;
+
+
+
+//static ArrayList<String> risposteErrate = new ArrayList<String>();
+
+public Domanda (String domanda, String rispostaEsatta,String rispostaErrata1,String rispostaErrata2,String rispostaErrata3){
+    this.domanda=domanda;
+    this.rispostaEsatta=rispostaEsatta;
+    this.rispostaErrata1=rispostaErrata1;
+    this.rispostaErrata2=rispostaErrata2;
+    this.rispostaErrata3=rispostaErrata3;
+
+
+    //  this.risposteErrate=risposteErrate;
+
+}
+
+
+    public static String getDomanda() {
+        return domanda;
+    }
+
+    public static String getRispostaEsatta() {
+        return rispostaEsatta;
+    }
+
+    public static String getRispostaErrata1() {
+        return rispostaErrata1;
+    }
+
+    public static String getRispostaErrata2() {
+        return rispostaErrata2;
+    }
+
+    public static String getRispostaErrata3() {
+        return rispostaErrata3;
+    }
+    // public static ArrayList<String> getRisposteErrate() {
+     ///   return risposteErrate;
+   //}
+}
--- app/src/main/java/com/example/threemath/MainActivity.java
+++ app/src/main/java/com/example/threemath/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.threemath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Domanda> quesito = new ArrayList<>();
+
+
+        Domanda domanda1 = new Domanda("Come ti chiami ","andrea ","giacomo","mario","amedeo");
+        Domanda domanda2 = new Domanda("Il tuo cognome  ","sessa ","landi","de paoli","brighi");
+
+        String x = domanda1.getDomanda();
+
+        quesito.add(domanda1);
+        quesito.add(domanda2);
+
+        String y = quesito.get(0).getDomanda();
+        String j = quesito.get(0).getRispostaEsatta();
+
+        Log log = null;
+        log.d("DEBUG","domanda in arrivo = "+ y);
+        log.d("DEBUG","domanda in arrivo-risposta esatta= "+ j);
+
+
+
     }
+
+
+
+
+
 }
+
