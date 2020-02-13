27e7af91283baa883ca2f041774f8bd93a631e20
andreasessa <andrea.sessa1995libero.it>
2020-02-13 02:09:11 +0100

    risolto problema dell'indice, aggiunto controllo risposte esatte, aggiundo aumento progressivo degli indici per visualizzare le domande

--- app/src/main/java/com/example/threemath/Domanda.java
+++ app/src/main/java/com/example/threemath/Domanda.java
@@ -3,60 +3,58 @@ package com.example.threemath;
 import java.util.ArrayList;
 
 public class Domanda {
-static String domanda ="";
-static String rispostaEsatta ="";
 
-static String rispostaErrata1;
-static String rispostaErrata2;
-static String rispostaErrata3;
-
+    private String domanda = "";
+    private String rispostaEsatta = "";
+    private String rispostaErrata1 = "";
+    private String rispostaErrata2 = "";
+    private String rispostaErrata3 = "";
 
 
 //static ArrayList<String> risposteErrate = new ArrayList<String>();
 
-public Domanda (String domanda, String rispostaEsatta,String rispostaErrata1,String rispostaErrata2,String rispostaErrata3){
-    this.domanda=domanda;
-    this.rispostaEsatta=rispostaEsatta;
-    this.rispostaErrata1=rispostaErrata1;
-    this.rispostaErrata2=rispostaErrata2;
-    this.rispostaErrata3=rispostaErrata3;
+    public Domanda(String domanda, String rispostaEsatta, String rispostaErrata1, String rispostaErrata2, String rispostaErrata3) {
+        this.domanda = domanda;
+        this.rispostaEsatta = rispostaEsatta;
+        this.rispostaErrata1 = rispostaErrata1;
+        this.rispostaErrata2 = rispostaErrata2;
+        this.rispostaErrata3 = rispostaErrata3;
 
 
-    //  this.risposteErrate=risposteErrate;
+        //  this.risposteErrate=risposteErrate;
 
-}
+    }
 
 
-    public static String getDomanda() {
+    public String getDomanda() {
         return domanda;
     }
 
-    public static String getRispostaEsatta() {
+    public String getRispostaEsatta() {
         return rispostaEsatta;
     }
 
-    public static String getRispostaErrata1() {
+    public String getRispostaErrata1() {
         return rispostaErrata1;
     }
 
-    public static String getRispostaErrata2() {
+    public String getRispostaErrata2() {
         return rispostaErrata2;
     }
 
-    public static String getRispostaErrata3() {
+    public String getRispostaErrata3() {
         return rispostaErrata3;
     }
     // public static ArrayList<String> getRisposteErrate() {
-     ///   return risposteErrate;
-   //}
+    ///   return risposteErrate;
+    //}
 
-    public boolean checkRisposta(String risposta){
-    boolean esito = false;
-        if (risposta.equalsIgnoreCase(rispostaEsatta)){
-            esito = true ;
-        }
-        else {
-            esito =  false;
+    public boolean checkRisposta(String risposta) {
+        boolean esito = false;
+        if (risposta.equalsIgnoreCase(rispostaEsatta)) {
+            esito = true;
+        } else {
+            esito = false;
         }
         return esito;
     }
--- app/src/main/java/com/example/threemath/MainActivity.java
+++ app/src/main/java/com/example/threemath/MainActivity.java
@@ -9,17 +9,27 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView testoDomanda ;
+    TextView testoDomanda;
     TextView testoCategoriaDomanda;
-    Button A ,B,C,D;
-    ArrayList<Domanda> quesito;
+    Button A, B, C, D;
+    ArrayList<Domanda> quesito = new ArrayList<>();
     int i = 0;
-    int numeroDomande = 3;
+    int sizeq = 0;
+    /**
+     * String domanda = " ";
+     * String rispostaCorretta = " ";
+     * String rispostaErrata1 = " ";
+     * String rispostaErrata2 = " ";
+     * String rispostaErrata3 = " ";
+     **/
 
-
+    ArrayList<Boolean> risposteQuesito = new ArrayList<>();
+    Random generatore = new Random();
+    int numRand = 0;
 
 
     @Override
@@ -27,57 +37,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // categoria domanda ( addizione sottrazione ...)
-        testoCategoriaDomanda =  ( TextView) findViewById(R.id.categoriaDomanda);
+        /** categoria domanda ( addizione sottrazione ...)**/
+        testoCategoriaDomanda = (TextView) findViewById(R.id.categoriaDomanda);
 
-        // testo domanda ( 50+4 ....)
-        testoDomanda =  (TextView) findViewById(R.id.corpoDomandaTesto);
+        /** testo domanda ( 50+4 ....)*/
+        testoDomanda = (TextView) findViewById(R.id.corpoDomandaTesto);
 
-        //bottoni 50-45-54-60 ....)
+        /**bottoni 50-45-54-60 ....)*/
         A = (Button) findViewById(R.id.A);
-        B= (Button) findViewById(R.id.B);
+        B = (Button) findViewById(R.id.B);
         C = (Button) findViewById(R.id.C);
         D = (Button) findViewById(R.id.D);
 
 
-         quesito = new ArrayList<>();
+        Domanda uno = new Domanda("Come ti chiami ", "andrea", "giacomo", "mario", "amedeo");
+        quesito.add(0, uno);
+        Domanda due = new Domanda("Il tuo cognome  ", "sessa", "landi", "de paoli", "brighi");
+        quesito.add(1, due);
+        Domanda tre = new Domanda("Il cognome di mamma  ", "sessa", "landi", "rossi", "brighi");
+        quesito.add(2, tre);
 
 
-        Domanda domanda1 = new Domanda("Come ti chiami ","andrea","giacomo","mario","amedeo");
-        Domanda domanda2 = new Domanda("Il tuo cognome  ","sessa","landi","de paoli","brighi");
-        Domanda domanda3 = new Domanda("Il cognome di mamma  ","sessa","landi","rossi","brighi");
+        //String x = domanda1.getDomanda();
 
 
+        sizeq = quesito.size();
 
-
-
-        String x = domanda1.getDomanda();
-
-        quesito.add(domanda1);
-        quesito.add(domanda2);
-        quesito.add(domanda3);
-        
-        int sizeq = quesito.size();
-
-       // String y = quesito.get(0).getDomanda();
-       // String j = quesito.get(0).getRispostaEsatta();
+        // String y = quesito.get(0).getDomanda();
+        // String j = quesito.get(0).getRispostaEsatta();
 
         Log log = null;
-     //   log.d("DEBUG","domanda in arrivo = "+ y);
-      //  log.d("DEBUG","domanda in arrivo-risposta esatta= "+ j);
+        //   log.d("DEBUG","domanda in arrivo = "+ y);
+        //  log.d("DEBUG","domanda in arrivo-risposta esatta= "+ j);
 
-     //   String risposta = "sessa";
+        //   String risposta = "sessa";
 
-      //  boolean esito = quesito.get(1).checkRisposta(risposta);
+        //  boolean esito = quesito.get(1).checkRisposta(risposta);
 
-     //   log.d("DEBUG","domanda  esito  Wwwwwwwwwwwwww= = "+ esito);
-        log.d("DEBUG","size quesito rrrrrrrrrrrrrrrrrr= = "+sizeq);
+        //   log.d("DEBUG","domanda  esito  Wwwwwwwwwwwwww= = "+ esito);
+        //  log.d("DEBUG", "size quesito rrrrrrrrrrrrrrrrrr= = " + sizeq);
+       /* domanda = quesito.get(i).getDomanda();
+        rispostaCorretta = quesito.get(i).getRispostaEsatta();
+        rispostaErrata1 = quesito.get(i).getRispostaErrata1();
+        rispostaErrata2 = quesito.get(i).getRispostaErrata2();
+        rispostaErrata3 = quesito.get(i).getRispostaErrata3();
+        log.d("DEBUG", "domanda    Wwwwwwwwwwwwww= = " + domanda + " indice " + i);
 
-        log.d("DEBUG","domanda  risposta corr  Wwwwwwwwwwwwww= = "+ quesito.get(i).getRispostaEsatta()+" indice "+i);
-        log.d("DEBUG","domanda  risposta err Wwwwwwwwwwwwww= = "+ quesito.get(i).getRispostaErrata1()+" indice "+i);
-        log.d("DEBUG","domanda  risposta err  Wwwwwwwwwwwwww= = "+ quesito.get(i).getRispostaErrata2()+" indice "+i);
-        log.d("DEBUG","domanda  rispostae rr  Wwwwwwwwwwwwww= = "+ quesito.get(i).getRispostaErrata3()+" indice "+i);
+        log.d("DEBUG", "domanda  risposta corr  Wwwwwwwwwwwwww= = " + rispostaCorretta + " indice " + i);
+        log.d("DEBUG", "domanda  risposta err Wwwwwwwwwwwwww= = " + rispostaErrata1 + " indice " + i);
+        log.d("DEBUG", "domanda  risposta err  Wwwwwwwwwwwwww= = " + rispostaErrata2 + " indice " + i);
+        log.d("DEBUG", "domanda  rispostae rr  Wwwwwwwwwwwwww= = " + rispostaErrata3 + " indice " + i);
 
+        */
 
 
         testoCategoriaDomanda.setText("Addizione");
@@ -89,43 +100,75 @@ public class MainActivity extends AppCompatActivity {
         C.setText(quesito.get(i).getRispostaErrata2());
         D.setText(quesito.get(i).getRispostaErrata3());
 
-        i = 1;
+        //  i = 1;
 
-
-
-
-
-
+        // if (i < sizeq) {
+        //    i++;
+        //  }
 
 
     }
-    public void onClickRisposta (View v){
-      //  boolean domandeTerminate=false;
+
+    public void onClickRisposta(View v) {
+        //  boolean domandeTerminate=false;
         Log log = null;
-        log.d("DEBUG","wwww333333333  Wwwwwwwwwwwwww= = "+i);
+        //  log.d("DEBUG", "wwww333333333  Wwwwwwwwwwwwww= = " + i);
+        //log.d("DEBUG", "wwww333333333qw  Wwwwwwwwwwwwww= = " + sizeq);
 
         //while(i<numDomande){
-            testoDomanda.setText(quesito.get(i).getDomanda());
-            if((A.isPressed()==true)||(B.isPressed()==true)||(C.isPressed()==true)||(D.isPressed()==true)){
-                A.setText(quesito.get(i).getRispostaEsatta());
-                B.setText(quesito.get(i).getRispostaErrata1());
-                C.setText(quesito.get(i).getRispostaErrata2());
-                D.setText(quesito.get(i).getRispostaErrata3());
-                A.setText("ooo");
-                log.d("DEBUG","domanda  risposta corr  Wwwwwwwwwwwwww= = "+ quesito.get(0).getRispostaEsatta()+" indice "+i);
-                log.d("DEBUG","domanda  risposta err Wwwwwwwwwwwwww= = "+ quesito.get(0).getRispostaErrata1()+" indice "+i);
-                log.d("DEBUG","domanda  risposta err  Wwwwwwwwwwwwww= = "+ quesito.get(0).getRispostaErrata2()+" indice "+i);
-                log.d("DEBUG","domanda  rispostae rr  Wwwwwwwwwwwwww= = "+ quesito.get(0).getRispostaErrata3()+" indice "+i);
-               if(i<numeroDomande-1){
-                   i++;
-               }
+
+        /** CHECK RISPOSTA E SALVATAGGIO CHECK **/
+        if (i < sizeq) {
+
+            if (A.isPressed()) {
+                risposteQuesito.add(quesito.get(i).checkRisposta(A.getText().toString()));
+            } else if (B.isPressed()) {
+                risposteQuesito.add(quesito.get(i).checkRisposta(B.getText().toString()));
+            } else if (C.isPressed()) {
+                risposteQuesito.add(quesito.get(i).checkRisposta(C.getText().toString()));
+            } else if (D.isPressed()) {
+                risposteQuesito.add(quesito.get(i).checkRisposta(D.getText().toString()));
             }
+            log.d("DEBUG", "wwww333333333  Wwwwwwwwwwwwww= presssedddd = " + risposteQuesito.get(i));
+        }
 
-        // }
+        numRand = 1 + generatore.nextInt(4);
+        //numRand = (int) (Math.random()*4);
+
+        if (i < sizeq - 1) {
+            i++;
+            testoDomanda.setText(quesito.get(i).getDomanda());
+            if ((A.isPressed()) || (B.isPressed()) || (C.isPressed()) || (D.isPressed())) {
+
+                switch (numRand) {
+                    case (1):
+                        A.setText(quesito.get(i).getRispostaEsatta());
+                        B.setText(quesito.get(i).getRispostaErrata1());
+                        C.setText(quesito.get(i).getRispostaErrata2());
+                        D.setText(quesito.get(i).getRispostaErrata3());
+                    case (2):
+                        B.setText(quesito.get(i).getRispostaEsatta());
+                        C.setText(quesito.get(i).getRispostaErrata1());
+                        D.setText(quesito.get(i).getRispostaErrata2());
+                        A.setText(quesito.get(i).getRispostaErrata3());
+
+                    case (3):
+                        C.setText(quesito.get(i).getRispostaEsatta());
+                        D.setText(quesito.get(i).getRispostaErrata1());
+                        A.setText(quesito.get(i).getRispostaErrata2());
+                        B.setText(quesito.get(i).getRispostaErrata3());
+                    case (4):
+                        D.setText(quesito.get(i).getRispostaEsatta());
+                        A.setText(quesito.get(i).getRispostaErrata1());
+                        B.setText(quesito.get(i).getRispostaErrata2());
+                        C.setText(quesito.get(i).getRispostaErrata3());
+                }
 
 
-
+            }
+        }
     }
+
 
 }
 
