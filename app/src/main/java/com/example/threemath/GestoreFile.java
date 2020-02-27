1ff071dd1f200e7a9950ed8a50635da791dda5bc
andreasessa <andrea.sessa1995libero.it>
2020-02-27 21:26:42 +0100

    Aggiunto xml statistiche

--- app/src/main/java/com/example/threemath/GestoreFile.java
+++ app/src/main/java/com/example/threemath/GestoreFile.java
@@ -9,124 +9,38 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.FileSystemNotFoundException;
 
 public class GestoreFile {
     Log log = null;
 
-    public static final String SCOREADDIZIONI = "punteggioLivelloAddizioni.dat";
 
-    /**
-     * Salva come formato stringa + ,  il punteggio totalizzato
-     * sommandolo al precedente.
-     * @param context
-     * @param punteggio
-     */
-    public void salvaScoresAddizioni(Context context, int punteggio) {
-        //log.d("DEBUG", "M'arrriv  puntegg  = " + punteggio);
+    public void creaFileScore(Context context) {
 
-        String spunteggio = ""+punteggio;
-
-        FileOutputStream fos = null;
-
-        try {
-            spunteggio += ",";
-            fos = context.openFileOutput(SCOREADDIZIONI, Context.MODE_PRIVATE);
-            fos.flush();
-            fos.write(spunteggio.getBytes());
-
-           // log.d("DEBUG", "Salvo questo sul file = " + spunteggio);
-            fos.close();
-           // log.d("DEBUG", "Leggo questo sul file  appena scritto = " + caricaScoresAddizioni(context));
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    /**
-     * carico il punteggio sottoforma di stringa + , la splitto e
-     * successivamente la converto in intero ritornando il valore dello score
-     * @param context
-     * @return
-     */
-    public int caricaScoresAddizioni(Context context) {
-
-        int score =0;
-        FileInputStream fis = null;
-        InputStreamReader isr = null;
-        String spunteggio= "";
-        String line = "";
-        String data ="";
-        try{
-            fis = context.openFileInput(SCOREADDIZIONI);
-            isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-
-            while (null != (line = br.readLine())) {
-                data += line;
-            }
-            br.close();
-            fis.close();
-
-            int x = data.indexOf(",");
-            String[] dati = data.split(",");
-            //score=br.read();
-            log.d("DEBUG", "Leggo questo dal file = = " + data);
-
-            spunteggio = dati[0];
-            score = Integer.parseInt(spunteggio);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return score;
-
-    }
-
-    /**
-     * azzero lo score nel file
-     * @param context
-     */
-    public void azzeraScoreAddizioni (Context context){
-        FileOutputStream fos = null;
-        try {
-            fos = context.openFileOutput(SCOREADDIZIONI, Context.MODE_PRIVATE);
-            fos.write("0,".getBytes());
-            fos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        salvaScores(context, 0, "Addizioni");
+        salvaScores(context, 0, "Sottrazioni");
+        salvaScores(context, 0, "Moltiplicazioni");
+        salvaScores(context, 0, "Divisioni");
 
     }
 
 
     /**
+     * salva il punteggio dello score della categoria passata
      *
      * @param context
      * @param punteggio
      * @param categoria
      */
-    public void salvaScores(Context context, int punteggio,String categoria) {
+    public void salvaScores(Context context, int punteggio, String categoria) {
 
 
-
-         String SCORE = "punteggioLivello"+categoria+".dat";
-
+        String SCORE = "punteggioLivello" + categoria + ".dat";
 
 
         //log.d("DEBUG", "M'arrriv  puntegg  = " + punteggio);
 
-        String spunteggio = ""+punteggio;
+        String spunteggio = "" + punteggio;
 
         FileOutputStream fos = null;
 
@@ -151,41 +65,48 @@ public class GestoreFile {
     }
 
     /**
+     * carica il punteggio dello score della categoria passata
      *
      * @param context
      * @param categoria
      * @return
      */
-    public int caricaScores(Context context,String categoria) {
+    public int caricaScores(Context context, String categoria) {
 
-        String SCORE = "punteggioLivello"+categoria+".dat";
+        String SCORE = "punteggioLivello" + categoria + ".dat";
 
-        int score =0;
+        int score = 0;
         FileInputStream fis = null;
         InputStreamReader isr = null;
-        String spunteggio= "";
+        String spunteggio = "";
         String line = "";
-        String data ="";
-        try{
-            fis = context.openFileInput(SCORE);
-            isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
+        String data = "";
+        try {
 
-            while (null != (line = br.readLine())) {
-                data += line;
-            }
-            br.close();
-            fis.close();
 
-            int x = data.indexOf(",");
-            String[] dati = data.split(",");
-            //score=br.read();
-            log.d("DEBUG", "Leggo questo dal file = = " + data);
 
-            spunteggio = dati[0];
-            score = Integer.parseInt(spunteggio);
+
+                fis = context.openFileInput(SCORE);
+                isr = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr);
+                while (null != (line = br.readLine())) {
+                    data += line;
+                }
+                br.close();
+                fis.close();
+
+                int x = data.indexOf(",");
+                String[] dati = data.split(",");
+                //score=br.read();
+                log.d("DEBUG", "Leggo questo dal file = = " + data);
+
+                spunteggio = dati[0];
+                score = Integer.parseInt(spunteggio);
 
         } catch (FileNotFoundException e) {
+            Log log =null;
+            log.d("DEBUG", "oooooooooooooo e nullllll il fileeeeeeee " );
+            azzeraScore(context, categoria);
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -196,12 +117,11 @@ public class GestoreFile {
     }
 
     /**
-     *
      * @param context
      */
-    public void azzeraScore (Context context,String categoria){
+    public void azzeraScore(Context context, String categoria) {
 
-        String SCORE = "punteggioLivello"+categoria+".dat";
+        String SCORE = "punteggioLivello" + categoria + ".dat";
 
         FileOutputStream fos = null;
         try {
@@ -215,7 +135,6 @@ public class GestoreFile {
         }
 
     }
-
 
 
 }
--- app/src/main/java/com/example/threemath/StatisticheActivity.java
+++ app/src/main/java/com/example/threemath/StatisticheActivity.java
@@ -1,12 +1,37 @@
 package com.example.threemath;
 
-import android.app.AppComponentFactory;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StatisticheActivity extends AppCompatActivity {
+
+
+
+    /**TExt view Punteggi*/
+
+    TextView textPunteggioAddizioni ;
+    TextView textPunteggioSottrazioni ;
+    TextView textPunteggioMoltiplicazioni ;
+    TextView textPunteggioDivisioni ;
+
+    TextView textRisposteCorrette;
+    TextView textRisposteErrate;
+
+    TextView textPercentualeRisposteCorrette;
+    TextView textPercentualeRisposteErrate;
+
+
+    GestoreFile gf ;
+    int punteggio =0;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +41,50 @@ public class StatisticheActivity extends AppCompatActivity {
 
 
 
+        /**Punteggi*/
+        TextView textPunteggioAddizioni =  findViewById(R.id.textPunteggioAddizioni);
+        TextView textPunteggioSottrazioni =  findViewById(R.id.textPunteggioSottrazioni);
+        TextView textPunteggioMoltiplicazioni = findViewById(R.id.textPunteggioMoltiplicazioni);
+        TextView textPunteggioDivisioni =  findViewById(R.id.textPunteggioDivisioni);
+
+        /**Risposte corrette ed errate*/
+        TextView textRisposteCorrette =  findViewById(R.id.textRisposteCorrette);
+        TextView textRisposteErrate =  findViewById(R.id.textRisposteErrate);
+
+        /**Risposte percentuale corrette ed errate*/
+        TextView textPercentualeRisposteCorrette =  findViewById(R.id.textPercentualeRisposteCorrette);
+        TextView textPercentualeRisposteErrate =  findViewById(R.id.textPercentualeRisposteErrate);
+
+
+        gf = new GestoreFile();
+
+        punteggio = loadPunteggi(getApplicationContext(),"Addizioni");
+        textPunteggioAddizioni.setText(""+punteggio);
+
+        punteggio = loadPunteggi(getApplicationContext(),"Sottrazioni");
+        textPunteggioSottrazioni.setText(""+punteggio);
+
+        punteggio = loadPunteggi(getApplicationContext(),"Moltiplicazioni");
+        textPunteggioMoltiplicazioni.setText(""+punteggio);
+
+        punteggio = loadPunteggi(getApplicationContext(),"Divisioni");
+        textPunteggioDivisioni.setText(""+punteggio);
 
 
 
+
+
+
+
+
+
+
+
+    }
+
+    private int  loadPunteggi(Context context, String categoria){
+
+        return gf.caricaScores(context,categoria);
 
     }
 }
